a87237fda67cd6170323fad80db46abb66bef07f
Rafael Henrique <contato@umaguin.com>
2020-08-08 14:27:55 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -11,8 +11,19 @@ const proffys = [
         time_to: [1220]
     },
     {
-        name: "Daniele Evangelista",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        name: "Mayk Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp: "75998537880",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Mayk Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
         whatsapp: "75998537880",
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
@@ -23,20 +34,29 @@ const proffys = [
     }
 ]
 
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageStudy(req, res) {
+    return res.render("study.html", {proffys})
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
 
 const express = require('express')
 const server = express()
 
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
 server.use(express.static("public"))
-
-    .get("/", (req, res) => {
-        return res.sendFile(__dirname + "/views/index.html")
-    })
-    .get("/study", (req, res) => {
-        return res.sendFile(__dirname + "/views/study.html")
-    })
-    .get("/give-classes", (req, res) => {
-        return res.sendFile(__dirname + "/views/give-classes.html")
-    })
-
+    .get("/", pageLanding)
+    .get("/study", pageStudy)
+    .get("/give-classes", pageGiveClasses)
     .listen(5500)
