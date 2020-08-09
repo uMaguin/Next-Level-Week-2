9f642af1178573416489d878a0c76c19c4ffde52
Rafael Henrique <contato@umaguin.com>
2020-08-09 09:01:59 -0300

    Update

---
+++ src/database/test.js
@@ -0,0 +1,10 @@
+const Database = require('./db')
+
+Database.then((db) => {
+    proffy = {
+        name: 'Mayk Brito',
+        avatar: 'https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4',
+        whatsapp: '75998537880',
+        bio: 'Instrutor de Educação Física'
+    }
+})
