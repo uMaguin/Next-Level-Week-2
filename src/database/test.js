e13845511385bfad625f243d5f0ea685d53f601b
Rafael Henrique <contato@umaguin.com>
2020-08-09 12:31:51 -0300

    Update

--- src/database/test.js
+++ src/database/test.js
@@ -10,7 +10,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: "Química",
+        subject: 1,
         cost: "20",
     }
 
@@ -27,5 +27,21 @@ Database.then(async (db) => {
         }
     ]
 
-    // await createProffy(db, {proffyValue, classValue, classScheduleValues})
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+
+    const selectedClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+
+    const selectClassesSchedules = await db.all (`
+    SELECT class_schedule.*
+    FROM class_schedule
+    WHERE class_schedule.class_id = "1"
+    AND class_schedule.weekday = "0"
+    AND class_schedule.time_from <= "1300"
+    AND class_schedule.time_to > "1300"
+    `)
 })
